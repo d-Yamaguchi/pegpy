3c51d7ec19d8b1eefed81902039e7385d78ce099
d-Yamaguchi <d.yamaguchi.bf@gmail.com>
2019-02-01 02:38:15 +0900

    fixed tf tree env tbcnn

--- pegpy/tbcnn/treeEmb_tf.py
+++ pegpy/tbcnn/treeEmb_tf.py
@@ -11,7 +11,7 @@ lower = 0.002
 samples = 100
 
 #################################
-# configure hyperparameter
+# configure hyper parameter
 margin = 1.0
 learnRate = .003
 momentum = 0.1
@@ -56,7 +56,7 @@ class VectorTree(object):
     def leafs(self):
         return list(filter(lambda item: len(item) == 0, self.serialize()))
 
-    def numberOfLeafs(self):
+    def numberOfLeaf(self):
         return len(self.leafs())
 
 # Parser
@@ -79,16 +79,15 @@ def parse2vector(parsetree, sibpos = 0, sibnum = 0):
     return top
 
 # tagのマップを作る
-tag2idx = {"Infix" : tf.constant([0,0,0,0,1]), " " : tf.constant([0,0,0,1,0]), "Int" : tf.constant([0,0,1,0,0]),"Plus" : tf.constant([0,1,0,0,0]),"Mul" : tf.constant([1,0,0,0,0])}
-tag2vec = tf.Variable(tf.random_uniform([feature_dimension,len(tag2idx)],lower,upper))
+tag2idx = {"Infix" : [0.0,0.0,0.0,0.0,1.0], " " : [0.0,0.0,0.0,1.0,0.0], "Int" : [0.0,0.0,1.0,0.0,0.0],"Plus" : [0.0,1.0,0.0,0.0,0.0],"Mul" : [1.0,0.0,0.0,0.0,0.0]}
+num_tag=len(tag2idx)
+tag2vec = tf.Variable(tf.random_uniform([feature_dimension,num_tag],lower,upper))
 
 fin = open(os.path.dirname(os.path.abspath(__file__)) + "/expressions.txt")
 progs = fin.readlines()
 fin.close()
 parsetrees = [parser(prog) for prog in progs]
 vectortrees = [parse2vector(parsetree) for parsetree in parsetrees]
-#trX, testX = train_test_split(np.array(vectortrees), train_size=0.8)
-trX = np.array(vectortrees)
 #################################
 # configure network 
 Wleft = tf.Variable(tf.random_uniform([feature_dimension,feature_dimension], lower, upper))
@@ -98,13 +97,33 @@ biase = tf.Variable(tf.random_uniform([feature_dimension], lower, upper))
 def matvecmul(mat, vec):
     return tf.matmul(mat,tf.expand_dims(vec,-1))
 
-def getVec(tagname):
-    val = matvecmul(tag2vec, tag2idx[tagname])
-    return tf.squeeze(val)
+def getVec(nodevec):
+    return tf.slice(nodevec,0,num_tag)
+'''
+tagIndex, Lf, Ns, Ps <-- nodeMatrix
+tagIndex, Lf, Ns, Ps
+tagIndex, Lf, Ns, Ps
+tagIndex, Lf, Ns, Ps
+tagIndex, Lf, Ns, Ps
+'''
+
+def nodeMatrix(node):
+    tail = [node.numberOfLeaf(), node.numberOfSiblings, node.positionOfSiblings]
+    return tag2idx[node.tag] + tail
+
+def inTensor(parsetree):
+    t = [nodeMatrix(node) for node in parsetree.serialize]
+    at = np.array(t)
+    return at.reshape([len(t),num_tag+3])
+
+trX, testX = train_test_split(np.array([inTensor(vect) for vect in vectortrees]), train_size=0.8)
+
+
+###Define:: Objective Function###
 
 def mulLandW(node):
     n = node.numberOfSiblings
-    l = node.numberOfLeafs()
+    l = node.numberOfLeaf()
 
     def ret(nodevector):
         if n == 0:# Unary tree だった場合
@@ -128,10 +147,10 @@ def j(vecTree):
         return 0.0
     else:
         # for positive
-        currentVector = getVec(vecTree.tag)
+        currentVector = getVec(vecTree)
         numOLeafs = vecTree.numberOfLeaf()
         mlw = [mulLandW(ch) for ch in vecTree.child]
-        nextVector = tf.nn.tanh((1.0/numOLeafs) * sum(mlw) + tf.expand_dims(biase,-1))
+        nextVector = tf.nn.tanh((1.0/numOLeafs) * tf.add_n(mlw) + tf.expand_dims(biase,-1))
         d_positive = tf.norm(currentVector - nextVector)
         #for negative
         negNodeIndex = np.random.randint(0, len(vecTree))
@@ -141,35 +160,32 @@ def j(vecTree):
             vecTree.child[negNodeIndex].isnegativesample = True
             neglw = [mulLandW(ch) for ch in vecTree.child]
             vecTree.child[negNodeIndex].isnegativesample = False
-            nextVector_neg = tf.nn.tanh(tf.scalar_mul(1/numOLeafs, sum(neglw)) + tf.expand_dims(biase,-1))
+            nextVector_neg = tf.nn.tanh(tf.scalar_mul(1/numOLeafs, tf.add_n(neglw)) + tf.expand_dims(biase,-1))
             d_negative = tf.norm(currentVector - tf.squeeze(nextVector_neg))
         er = margin + d_positive - d_negative
         return tf.math.maximum(0.0,er)
 
-def objective(vectree_idx):
-    vectree = trX[vectree_idx]
-    treeList = vectree.serialize()
-    totalError = sum([j(tree) for tree in treeList]) * 0.5 / len(treeList)
+def objective(tree_tensor):
+    totalError = tf.add_n([j(tree) for tree in tree_tensor]) * 0.5 / len(tree_tensor)
     regularizer = (lam * 0.5 * (tf.norm(Wleft) + tf.norm(Wright) )) / pow(feature_dimension, 2)
     return totalError + regularizer
 
 ### Model Parameters
 X = tf.placeholder(tf.float32)
+
 train_step = tf.train.MomentumOptimizer(learnRate,momentum).minimize(objective(X))
 
 ### Session
-#BATCH_SIZE = 20
+BATCH_SIZE = 20
 TRAIN_EPOCH = 10
 
 with tf.Session() as sess:
     tf.global_variables_initializer()
-    idxarray = np.array(range(len(trX)))
-    # training SGD
     for epoch in range(TRAIN_EPOCH):
-        np.random.shuffle(idxarray)
-        for idx in idxarray:
-            sess.run(train_step, feed_dict={X:idx})
-            print(idx)
+        p = np.random.permutation(range(len(trX)))
+        trX = trX[p]
+        for start in range(0, len(trX), BATCH_SIZE):
+            end = start + BATCH_SIZE
         #minibatch
         # p = np.random.permutation(range(len(trX)))
         # trX = trX[p]
